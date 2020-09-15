d0ba4ef43067171959d239a4c4bbcaae593eadb2
MakolaMak <makolamak@gmail.com>
2020-09-15 11:26:04 +0300

    Updates

---
+++ precache-manifest.e0cccdcfac8611f866f6965b2fcb4413.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eab8fafff421c22efd0c28292927b4b5",
+    "url": "/abz-test/index.html"
+  },
+  {
+    "revision": "7f718c4ccfb2dac7efed",
+    "url": "/abz-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4509e7430412f6f7777f",
+    "url": "/abz-test/static/js/2.5b159368.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/abz-test/static/js/2.5b159368.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f718c4ccfb2dac7efed",
+    "url": "/abz-test/static/js/main.2eaa21b6.chunk.js"
+  },
+  {
+    "revision": "c61ad13c423c1a636e97",
+    "url": "/abz-test/static/js/runtime-main.fb808539.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/abz-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
